83ccd529999d2f2ea9dadd499131fadac874b38c
David Adamson <david.adamson@telekom.de>
2022-12-06 23:10:00 +0100

    adjust test template

--- src/2022/7/1/level.test.ts
+++ src/2022/7/1/level.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from "@jest/globals";
 import StringStream from "../../../utils/string-stream";
 import Level from "./level";
-import { readFile as _readFile, writeFile } from "fs/promises";
+import { readFile as _readFile, writeFile as _writeFile } from "fs/promises";
 import { existsSync } from "fs";
 import { testLevel, testName } from "../../../utils/test-utils";
 import AdventOfCodeApi from "../../../utils/advent-of-code-api";
@@ -12,6 +12,7 @@ describe(testLevel(__dirname).join("::"), () => {
   const adventOfCodeApi = new AdventOfCodeApi(env.SESSION_ID);
   const inputFile = () => `${__dirname}/${testName()}`;
   const readFile = () => _readFile(inputFile(), "utf8");
+  const writeFile = (data: string) => _writeFile(inputFile(), data);
 
   test("example.txt", async () => {
     const input = new StringStream(await readFile());
@@ -22,10 +23,9 @@ describe(testLevel(__dirname).join("::"), () => {
 
   test("../prod.txt", async () => {
     const [year, day, level] = testLevel(__dirname);
-    if (!existsSync(inputFile())) await writeFile(inputFile(), await adventOfCodeApi.getInput(year, day));
+    if (!existsSync(inputFile())) await writeFile(await adventOfCodeApi.getInput(year, day));
     const input = new StringStream(await readFile());
     const output = new Level(input).run();
-    console.log(`answer: ${output}`);
     if ((await question(`submit output (${output})?: `)) === "y")
       await adventOfCodeApi.submitOutput(year, day, level, output);
   });
