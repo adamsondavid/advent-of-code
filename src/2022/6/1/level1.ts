b922ff9e7e479f6dd6838eab87b3c3cd9bd48819
David Adamson <david.adamson@telekom.de>
2022-12-07 10:23:21 +0100

    refactor

--- src/2022/6/1/level1.ts
+++ src/2022/6/1/level1.ts
@@ -1,7 +1,18 @@
 import Level from "../../../utils/level";
 
 export default class extends Level {
-  public run(): string {
-    throw new Error("level not implemented");
+  public run() {
+    const input = this.input.readLine()!;
+
+    const buffer = [];
+    for (let i = 0; i < input.length; i++) {
+      if (buffer.length === 4) buffer.shift();
+      buffer.push(input[i]);
+      if (buffer.length === 4) {
+        if ([...new Set(buffer)].length === 4) {
+          return i + 1;
+        }
+      }
+    }
   }
 }
