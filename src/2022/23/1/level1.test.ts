78ae452cc95a31c7d3190e85e4f3a8563aad47f5
David Adamson <david.adamson@telekom.de>
2022-12-23 07:30:15 +0100

    day23

--- src/2022/23/1/level1.test.ts
+++ src/2022/23/1/level1.test.ts
@@ -14,11 +14,11 @@ describe("level1", () => {
 
   test("../example.txt", async () => {
     const output = level.run();
-    expect(output).toBe("TODO");
+    expect(output).toBe(110);
   });
 
   test("../prod.txt", async () => {
     const output = level.run();
-    expect(output).toBe("TODO");
+    expect(output).toBe(3925);
   });
 });
