fb9fd620fa38d9e5061df8810fb2f148262d2f35
David Adamson <david.adamson2304@gmail.com>
2023-12-02 06:50:20 +0100

    day 2

--- src/2023/2/1/level1.test.ts
+++ src/2023/2/1/level1.test.ts
@@ -14,11 +14,11 @@ describe("level1", () => {
 
   test("../example.txt", async () => {
     const output = level.run();
-    expect(output).toBe("TODO");
+    expect(output).toBe(8);
   });
 
   test("../prod.txt", async () => {
     const output = level.run();
-    expect(output).toBe("TODO");
+    expect(output).toBe(2545);
   });
 });
