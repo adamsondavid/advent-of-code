3a006ee342738425e05406be241e270e6e1d11a2
David Adamson <david.adamson2304@gmail.com>
2023-12-04 07:25:33 +0100

    day4

--- src/2023/4/1/level1.test.ts
+++ src/2023/4/1/level1.test.ts
@@ -10,12 +10,12 @@ describe("level1", () => {
   test("../example.txt", async () => {
     const input = await readInput();
     const output = solve(input);
-    expect(output).toBe("TODO");
+    expect(output).toBe(13);
   });
 
   test("../prod.txt", async () => {
     const input = await readInput();
     const output = solve(input);
-    expect(output).toBe("TODO");
+    expect(output).toBe(25571);
   });
 });
