fb89d102b140b08f79e8c75f65e9fd5910b3cd7f
David Adamson <david.adamson2304@gmail.com>
2023-12-08 06:52:14 +0100

    day8

--- src/2023/8/2/level2.test.ts
+++ src/2023/8/2/level2.test.ts
@@ -7,15 +7,63 @@ import { testName } from "../../../utils/test-utils";
 describe("level2", () => {
   const readInput = async () => new StringStream(await readFile(`${__dirname}/${testName()}`, "utf8"));
 
-  test("../example.txt", async () => {
+  test("example.txt", async () => {
     const input = await readInput();
     const output = solve(input);
-    expect(output).toBe("TODO");
+    expect(output).toEqual({
+      solution: "lcm(2,3)",
+      nodes: [
+        {
+          start: "11A",
+          end: "11Z",
+          steps: 2,
+        },
+        {
+          start: "22A",
+          end: "22Z",
+          steps: 3,
+        },
+      ],
+    });
   });
 
   test("../prod.txt", async () => {
     const input = await readInput();
     const output = solve(input);
-    expect(output).toBe("TODO");
+    expect(output).toEqual({
+      solution: "lcm(18961,12169,17263,13301,14999,16697)",
+      nodes: [
+        {
+          start: "BGA",
+          end: "LCZ",
+          steps: 18961,
+        },
+        {
+          start: "SLA",
+          end: "LQZ",
+          steps: 12169,
+        },
+        {
+          start: "PTA",
+          end: "SNZ",
+          steps: 17263,
+        },
+        {
+          start: "AAA",
+          end: "ZZZ",
+          steps: 13301,
+        },
+        {
+          start: "XJA",
+          end: "PDZ",
+          steps: 14999,
+        },
+        {
+          start: "JNA",
+          end: "PBZ",
+          steps: 16697,
+        },
+      ],
+    });
   });
 });
