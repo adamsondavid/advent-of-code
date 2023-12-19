c39174571bd96f2b1503ca418ab7e65870561f9c
David Adamson <david.adamson2304@gmail.com>
2023-12-19 06:48:25 +0100

    day19 part1

--- src/2023/19/1/level1.ts
+++ src/2023/19/1/level1.ts
@@ -1,3 +1,37 @@
 import StringStream from "../../../utils/string-stream";
 
-export function solve(input: StringStream) {}
+type Part = { x: number; m: number; a: number; s: number };
+
+export function solve(input: StringStream) {
+  const [rulesString, partsString] = input.readLines().join("\n").split("\n\n");
+
+  const rules = Object.fromEntries(
+    rulesString.split("\n").map((line) => [
+      line.split("{")[0],
+      new Function(
+        "part",
+        "const { x, m, a, s } = part; return " +
+          line
+            .split("{")[1]
+            .replaceAll(":", "?")
+            .replaceAll(",", ":")
+            .replaceAll("}", "")
+            .replaceAll(/([a-z]{2,}|[AR])/g, "'$1'"),
+      ) as (part: Part) => string,
+    ]),
+  );
+
+  const parts = partsString
+    .split("\n")
+    .map((line) => JSON.parse(line.replaceAll("=", ":").replaceAll(/([xmas])/g, '"$1"')) as Part);
+
+  return parts
+    .map((part) => {
+      let status = "in";
+      while (!["A", "R"].includes(status)) status = rules[status](part);
+      return { part, status };
+    })
+    .filter(({ status }) => status === "A")
+    .map(({ part }) => part.x + part.m + part.a + part.s)
+    .reduce((sum, partValue) => sum + partValue);
+}
