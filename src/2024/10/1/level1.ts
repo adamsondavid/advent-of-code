96eb7d30c705acd4ffbc79fd5d93ad0bac0f6ca7
David Adamson <david.adamson2304@gmail.com>
2024-12-10 06:21:11 +0100

    day10

--- src/2024/10/1/level1.ts
+++ src/2024/10/1/level1.ts
@@ -1,3 +1,29 @@
 import StringStream from "../../../utils/string-stream";
 
-export function solve(input: StringStream) {}
+export function solve(input: StringStream) {
+  const map = input.readLines().map((line) => line.split("").map(Number));
+
+  const starts: { x: number; y: number }[] = [];
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] === 0) starts.push({ x, y });
+    }
+  }
+
+  let res = 0;
+  for (const start of starts) {
+    const queue = [start];
+    const ends = new Set();
+    while (queue.length > 0) {
+      const current = queue.shift()!;
+      const currentHeight = map[current.y][current.x];
+      if (currentHeight === 9) ends.add(JSON.stringify(current));
+      if (map[current.y + 1]?.[current.x] === currentHeight + 1) queue.push({ x: current.x, y: current.y + 1 });
+      if (map[current.y][current.x + 1] === currentHeight + 1) queue.push({ x: current.x + 1, y: current.y });
+      if (map[current.y - 1]?.[current.x] === currentHeight + 1) queue.push({ x: current.x, y: current.y - 1 });
+      if (map[current.y][current.x - 1] === currentHeight + 1) queue.push({ x: current.x - 1, y: current.y });
+    }
+    res += ends.size;
+  }
+  return res;
+}
