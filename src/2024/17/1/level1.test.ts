4d2bcb7306e854d3db5b0050d09690ad35bde1f8
David Adamson <david.adamson2304@gmail.com>
2024-12-17 09:29:42 +0100

    day17 p1

--- src/2024/17/1/level1.test.ts
+++ src/2024/17/1/level1.test.ts
@@ -1,17 +1,48 @@
 import { describe, expect, test } from "vitest";
-import { solve } from "./level1";
+import { runProgram, solve } from "./level1";
 import { useInput, useProdInput } from "../../../utils/test-utils";
 
 describe("level1", () => {
   test("../example.txt", async () => {
     const input = await useInput(__dirname);
     const output = solve(input);
-    expect(output).toBe("TODO");
+    expect(output).toBe("4,6,3,5,6,3,5,2,1,0");
   });
 
   test("../prod.txt", async () => {
     const input = await useProdInput(__dirname);
     const output = solve(input);
-    expect(output).toBe("TODO");
+    expect(output).toBe("1,2,3,1,3,2,5,3,1");
+  });
+
+  test("program 2,6 with c=9", () => {
+    const registers = { a: 0, b: 0, c: 9 };
+    runProgram([2, 6], registers);
+    expect(registers.b).toBe(1);
+  });
+
+  test("program 5,0,5,1,5,4 with a=10", () => {
+    const registers = { a: 10, b: 0, c: 0 };
+    const stdout = runProgram([5, 0, 5, 1, 5, 4], registers);
+    expect(stdout).toEqual([0, 1, 2]);
+  });
+
+  test("program 0,1,5,4,3,0 with a=2024", () => {
+    const registers = { a: 2024, b: 0, c: 0 };
+    const stdout = runProgram([0, 1, 5, 4, 3, 0], registers);
+    expect(registers.a).toBe(0);
+    expect(stdout).toEqual([4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0]);
+  });
+
+  test("program 1,7 with b=29", () => {
+    const registers = { a: 0, b: 29, c: 0 };
+    const stdout = runProgram([1, 7], registers);
+    expect(registers.b).toBe(26);
+  });
+
+  test("program 1,7 with b=2024 and c=43690", () => {
+    const registers = { a: 0, b: 2024, c: 43690 };
+    const stdout = runProgram([4, 0], registers);
+    expect(registers.b).toBe(44354);
   });
 });
--- src/2024/17/1/level1.ts
+++ src/2024/17/1/level1.ts
@@ -1,3 +1,44 @@
 import StringStream from "../../../utils/string-stream";
 
-export function solve(input: StringStream) {}
+export function runProgram(program: number[], registers: { a: number; b: number; c: number }) {
+  let ir = 0;
+  const stdout: number[] = [];
+
+  while (program[ir] !== undefined) {
+    const opcode = program[ir];
+    const literalOperand = program[ir + 1];
+
+    const comboOperand = () => {
+      if (literalOperand >= 0 && literalOperand <= 3) return literalOperand;
+      else if (literalOperand === 4) return registers.a;
+      else if (literalOperand === 5) return registers.b;
+      else if (literalOperand === 6) return registers.c;
+      else throw new Error(`unknown combo operand for literal operand: ${literalOperand}`);
+    };
+
+    if (opcode === 0) registers.a = Math.floor(registers.a / 2 ** comboOperand());
+    else if (opcode === 1) registers.b = registers.b ^ literalOperand;
+    else if (opcode === 2) registers.b = comboOperand() % 8 & 7;
+    else if (opcode === 3 && registers.a !== 0) {
+      ir = literalOperand;
+      continue;
+    } else if (opcode === 4) registers.b = registers.b ^ registers.c;
+    else if (opcode === 5) stdout.push(comboOperand() % 8);
+    else if (opcode === 6) registers.b = Math.floor(registers.a / 2 ** comboOperand());
+    else if (opcode === 7) registers.c = Math.floor(registers.a / 2 ** comboOperand());
+
+    ir += 2;
+  }
+
+  return stdout;
+}
+
+export function solve(_input: StringStream) {
+  const a = Number(_input.readLine()!.match(/\d+/)![0]);
+  const b = Number(_input.readLine()!.match(/\d+/)![0]);
+  const c = Number(_input.readLine()!.match(/\d+/)![0]);
+  _input.readLine();
+  const program = _input.readLine()!.match(/\d+/g)!.map(Number);
+
+  return runProgram(program, { a, b, c }).join(",");
+}
