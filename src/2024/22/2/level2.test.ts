42daf629918a0af6cc89df78225e505a1691b99d
David Adamson <david.adamson2304@gmail.com>
2024-12-22 21:09:34 +0100

    day22 p2

--- src/2024/22/2/level2.test.ts
+++ src/2024/22/2/level2.test.ts
@@ -3,15 +3,15 @@ import { solve } from "./level2";
 import { useInput, useProdInput } from "../../../utils/test-utils";
 
 describe("level2", () => {
-  test("../example.txt", async () => {
+  test("../example2.txt", async () => {
     const input = await useInput(__dirname);
     const output = solve(input);
-    expect(output).toBe("TODO");
+    expect(output).toBe(23);
   });
 
   test("../prod.txt", async () => {
     const input = await useProdInput(__dirname);
     const output = solve(input);
-    expect(output).toBe("TODO");
+    expect(output).toBe(2223);
   });
 });
--- src/2024/22/2/level2.ts
+++ src/2024/22/2/level2.ts
@@ -1,3 +1,40 @@
 import StringStream from "../../../utils/string-stream";
 
-export function solve(input: StringStream) {}
+function mix(oldSecretNumber: bigint, newSecretNumber: bigint) {
+  return oldSecretNumber ^ newSecretNumber;
+}
+
+function prune(secretNumber: bigint) {
+  return secretNumber % 16777216n;
+}
+
+function nextSecretNumber(secretNumber: bigint) {
+  secretNumber = prune(mix(secretNumber, secretNumber * 64n));
+  secretNumber = prune(mix(secretNumber, secretNumber / 32n));
+  return prune(mix(secretNumber, secretNumber * 2048n));
+}
+
+export function solve(input: StringStream) {
+  const m = new Map<string, number>();
+  for (const secret of input.readLines().map(BigInt)) {
+    const prices = [Number(secret % 10n)];
+    let current = secret;
+    for (let i = 0; i < 2000; i++) {
+      current = nextSecretNumber(current);
+      prices.push(Number(current % 10n));
+    }
+    const seenSequences = new Set<string>();
+    for (let i = 4; i < prices.length; i++) {
+      const seq = [
+        prices[i - 3] - prices[i - 4],
+        prices[i - 2] - prices[i - 3],
+        prices[i - 1] - prices[i - 2],
+        prices[i] - prices[i - 1],
+      ].join(",");
+      if (seenSequences.has(seq)) continue;
+      seenSequences.add(seq);
+      m.set(seq, (m.get(seq) ?? 0) + prices[i]);
+    }
+  }
+  return Math.max(...m.values());
+}
